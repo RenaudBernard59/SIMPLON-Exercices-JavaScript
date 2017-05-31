2ebce6d0979a627ba310107d80c782375efa9866
RenaudBernard59 <renaud.bernard@p59production.com>
2017-05-31 15:35:39 +0200

    Part1 Exo4 FINISH

---
+++ Part1/Js/Exo4.js
@@ -0,0 +1,9 @@
+/* ========== Partie 1 - Exercice 4 ========== */
+
+var prenom = prompt("Quel est ton prénom : ");
+var nom = prompt("Quel est ton nom : ");
+
+
+alert("Tu t'appelles " + prenom + " " + nom + " !");
+
+// END
