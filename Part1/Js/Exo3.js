ef419765f6d21d73bc2d04fef07916b5b92f7f53
RenaudBernard59 <renaud.bernard@p59production.com>
2017-05-31 15:35:23 +0200

    Part1 Exo3 FINISH

---
+++ Part1/Js/Exo3.js
@@ -0,0 +1,7 @@
+/* ========== Partie 1 - Exercice 3 ========== */
+
+var prenom = prompt("Donnez-moi votre prenom : ");
+
+alert("Hein, je connais ton nom maintenant. Alors " + prenom + " comment vas tu ?");
+
+// END
