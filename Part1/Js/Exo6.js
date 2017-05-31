e5d6f397f89c2d14dbccb3e9bbf80bf12fcc8066
RenaudBernard59 <renaud.bernard@p59production.com>
2017-05-31 15:48:24 +0200

    Part1 Exo6 FINISH

---
+++ Part1/Js/Exo6.js
@@ -0,0 +1,10 @@
+/* ========== Partie 1 - Exercice 6 ========== */
+
+var monTexte = prompt("Ecrit ici un texte avec des majuscules et des minuscules :");
+
+monTexte = monTexte.toUpperCase();
+
+alert(monTexte);
+
+// END
+
