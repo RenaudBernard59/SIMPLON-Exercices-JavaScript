90d6822c67e2f1c6e597690aee25735b32118b38
RenaudBernard59 <renaud.bernard@p59production.com>
2017-05-31 16:52:15 +0200

    Part2 Exo1 FINISH

--- Part2/Js/Exo1.js
+++ Part2/Js/Exo1.js
@@ -1,9 +1,23 @@
 /* ========== Partie 2 - Exercice 1 ========== */
+nombre1 = Number(prompt("Rentre ici un 1er nombre :"));
+nombre2 = Number(prompt("Rentre ici un 2nd nombre :"));
+nombre3 = Number(prompt("Rentre ici un 3ème nombre :"));
+nombre4 = Number(prompt("Rentre ici un 4ème nombre :"));
 
+function testPlusGrand(nb1, nb2, nb3, nb4)
+{
+  if (nb1 > nb2 && nb1 > nb3 && nb1 > nb4) {
+    alert("Le plus grand est : " + nb1);
+  } else if  (nb2 > nb1 && nb2 > nb3 && nb2 > nb4)  {
+    alert("Le plus grand et : " + nb2);
+  } else if  (nb3 > nb1 && nb3 > nb2 && nb3 > nb4)  {
+    alert("Le plus grand est : " + nb3);
+  } else {
+    alert("Le plus grand est : " + nb4);
+  }
+}
 
-
-
-
+testPlusGrand(nombre1, nombre2, nombre3, nombre4)
 
 
 // END
