34717cc66721a151cd79c99dd38d9bff3c0e2bcc
RenaudBernard59 <renaud.bernard@p59production.com>
2017-06-02 14:51:04 +0200

    Part2 Exo3 BOGUE

--- Part2/Js/Exo3.js
+++ Part2/Js/Exo3.js
@@ -1,9 +1,58 @@
 /* ========== Partie 2 - Exercice 3 ========== */
+var nb;
+var nbDevinette;
+
+// Définir les fonctions
+function demanderNombre() {
+  nb = Number(prompt("Entrez un entier : "));
+  return nb;
+}
+
+function demanderNombreDevinette() {
+  nbDevinette = Number(prompt("Entrez un entier : "));
+  return nbDevinette;
+}
 
 
+function verifyEnter(nb) {
+  // Vérifier si Entier
+  var entier
+  function isInt(nb) {
+    entier = (n % 1 === 0);
+    return entier
+  }
+  // Agir en fonction
+  if (entier == true) {
+    demanderNombreDevinette();
+    jeuDevinette(nb);
+  } else {
+    demanderNombre();
+  }
 
-
-
-
-
+}
+  
+function jeuDevinette(nb, nbDevinette) {
+  while (nb != nbDevinette) {
+     if (nb > nbDevinette) {
+       alert("Choisis un nombre plus grand");
+       demanderNombreDevinette();     
+     } else if (nb > nbDevinette) {
+       alert("Choisis un nombre plus petit");
+       demanderNombreDevinette();
+     } else {
+       alert("Bravo tu as gagner !!!");
+     }
+  }
+}
+  
+  
+  
+  
+  
+// Lancer les fonctions
+demanderNombre();
+verifyEnter(nb);
+  
+  
 // END
+// git commit -am "Part2 Exo3 FINISH"
