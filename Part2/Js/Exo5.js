98378d1f4ecfda65a60549a94457532294e45918
RenaudBernard59 <renaud.bernard@p59production.com>
2017-06-02 15:30:32 +0200

    Part2 Exo5 FINISH

---
+++ Part2/Js/Exo5.js
@@ -0,0 +1,12 @@
+/* ========== Partie 2 - Exercice 5 ========== */
+var pair
+
+for (var i = 1; i <= 100; i++) {
+    if (i%2 ==0) {
+      //Il est pair
+      console.log(i);
+     }
+  }
+
+
+// END
