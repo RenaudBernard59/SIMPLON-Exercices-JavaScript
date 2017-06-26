9a1515d2bee1b244faab5376617834ebb96ce692
RenaudBernard59 <renaud.bernard@p59production.com>
2017-06-26 16:11:52 +0200

    Trying Part5 Exo2

--- Part5/Js/Exo2.js
+++ Part5/Js/Exo2.js
@@ -1,6 +1,19 @@
 /* ========== Partie 5 - Exercice 2 ========== */
 
+function table() {
+  nb = Number(prompt("Ajoutez un nombre pour la table de multiplication"));
+  var x = 1
+   while (x<11) {
+    tb = x * nb;
+    console.log(nb + " x " + x + " = " + tb);
+     x++;
+  }
+  
+  
+  
+}
 
+table();
 
 
 
