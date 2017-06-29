5db6bbec39b57ccf0fc0c31815b09b642a082b4e
RenaudBernard59 <renaud.bernard@p59production.com>
2017-06-29 15:50:36 +0200

    Part2 Exo7 FINISH

--- Part2/Js/Exo7.js
+++ Part2/Js/Exo7.js
@@ -1,9 +1,32 @@
 /* ========== Partie 2 - Exercice 7 ========== */
 
 
+function calcAireCerle(rayon) {
+	// Pi ère Carré
+	aireCercle = Math.pow((pi * rayon), 2);
+	return aireCercle;
+}
 
+function calcPerimetreCercle(rayon) {
+	// 2 Pi ère
+	perimetreCercle = 2 * pi * rayon;
+	return perimetreCercle;
+}
 
+function askMeSomething() {
+	pi = 3.14159265359;
+	rayon = parseInt(prompt("Entrez le rayon du cercle"));
 
+	calcAireCerle(rayon);
+	calcPerimetreCercle(rayon);
 
+	window.alert("Ton cercle de rayon " + rayon + ", a une aire de " 
+		+ aireCercle + " et un pérmiètre de "
+		+ perimetreCercle + "."
+		)
+
+}
+
+askMeSomething()
 
 // END
