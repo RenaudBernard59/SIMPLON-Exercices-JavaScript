90682a657170efb4a2d9a44a4445c35f66d98144
RenaudBernard59 <renaud.bernard@p59production.com>
2017-06-29 16:47:31 +0200

    MAKE Part3 Exo1 NOT Finish

--- Part3/Js/Exo1.js
+++ Part3/Js/Exo1.js
@@ -1,8 +1,31 @@
 /* ========== Partie 3 - Exercice 1 ========== */
+//Echequer = 8 * 8 < 1-1 = blanc
+noir = "■"
+blanc = "□"
+chaine = ""
+var i
+
+makeChaine = function(noir, blanc, chaine, i) {
+	if (i % 2 === 0){
+		// Si i est paire
+		chaine + chaine + noir;
+		return chaine
+	} else {
+		chaine + chaine + blanc;
+		return chaine;
+	};
+}
 
 
-
-
+for(i = 1; i < 65; i++) {
+	if (i % 8 === 0) {
+		makeChaine;
+		console.log(chaine);
+		chaine = "";
+	} else {
+		makeChaine;
+	} 
+}
 
 
 
