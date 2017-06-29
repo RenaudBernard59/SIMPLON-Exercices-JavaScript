32c9406f94e436aabdd0976a121fb12bea9a3a89
RenaudBernard59 <renaud.bernard@p59production.com>
2017-06-29 16:07:37 +0200

    Part2 Exo9 FINISH

--- Part2/Js/Exo9.js
+++ Part2/Js/Exo9.js
@@ -1,9 +1,25 @@
 /* ========== Partie 2 - Exercice 9 ========== */
 
+for (var i = 1; i <= 100; i++) {
+	 
+	if(i % 15 === 0) {
+		console.log("BUZZ");
+	} else if (i % 3 === 0) {
+		console.log("FIZZ");
+	} else {
+		console.log(i);
+	}
+
+ 
+
+}
 
 
 
 
 
 
-// END
+
+
+
+// END 
