766699af15656de90f2a1edccc3e01acc6df85a1
RenaudBernard59 <renaud.bernard@p59production.com>
2017-07-03 10:45:37 +0200

    END Part3 Exo5

--- Part3/Js/Exo5.js
+++ Part3/Js/Exo5.js
@@ -14,9 +14,35 @@ if "-" > "_"
 
  */
 
+function chaineConverter() {
+	maChaine = prompt("Donne moi une chaine de caractère quelquonque avec des '-' et des '_' !");
+	var tableTemporaire = [];
+	var chaineRetournee = "";
+	l = maChaine.length;
+	for (i = 0; i <= l; i++) {
+	// On découpe la chaine
+		tableTemporaire[i] = maChaine.charAt(i);
+			if (tableTemporaire[i] === "-") {
+				tableTemporaire[i] = "_";
+			};
+
+	};
+
+	// On recompose la chaine
+	for (a = 0; a <= l; a++) {
+		chaineRetournee = chaineRetournee + tableTemporaire[a]
+	};
+	alert(chaineRetournee);
+
+}
 
 
 
 
 
+
+
+
+
+chaineConverter();
 // END
