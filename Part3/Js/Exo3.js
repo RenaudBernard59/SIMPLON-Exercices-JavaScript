4a85300420c73eb0566af76a8baf11d3d39d2d3f
RenaudBernard59 <renaud.bernard@p59production.com>
2017-07-03 10:08:08 +0200

    END Part3 Exo3

--- Part3/Js/Exo3.js
+++ Part3/Js/Exo3.js
@@ -10,7 +10,17 @@ if num % 2 = 0 c'est OK
 
 
 
+function estPaire() {
+	monNombre = parseInt(prompt("Donne moi un nombre !"));
 
+	if (monNombre % 2 === 0) {
+		alert("Ce nombre est paire !");
+	} else {
+		alert("Ce nombre est impaire !");
+	}
+}
+
+estPaire();
 
 
 // END
