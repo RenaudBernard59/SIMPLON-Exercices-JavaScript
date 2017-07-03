762871d2eebd1882438b876f790fae32599c5221
RenaudBernard59 <renaud.bernard@p59production.com>
2017-07-03 12:18:00 +0200

    END Part3 Exo7

--- Part3/Js/Exo7.js
+++ Part3/Js/Exo7.js
@@ -20,6 +20,26 @@ onjet liste personne = {
 }
 */
 
+function unHomme() {
+	lHomme = [];
+	lHomme.push("BERNARD");
+	lHomme.push("Renaud");
+	lHomme.push(25);
+	lHomme.push("31/01/1992");
+
+	console.log(lHomme);
+
+	lAutre1 = ["HAD", "Dad", 33, "21/12/2012"];
+	lAutre2 = ["BIIB", "Fasaioda", 21, "01/01/2000"];
+	lAutre3 = ["SAVA", "Bien", 66, "12/12/2012"];
+
+	lesGens = [lHomme, lAutre1, lAutre2, lAutre3];
+
+	console.log(lesGens);
+}
+
+
+unHomme()
 
 
 
