45b89d9a61cb238b4ecc96fd6c1dfc04ef20c206
RenaudBernard59 <renaud.bernard@p59production.com>
2017-07-03 10:03:53 +0200

    END Part3 Exo2

--- Part3/Js/Exo2.js
+++ Part3/Js/Exo2.js
@@ -18,5 +18,52 @@ du coup table[1] deviens table[0]
 0
 "--------"
 */
+/* Version SIMPLE mais la console n'affiche pas bien "repeat"
+var maTable = [1, 0, 0, 0];
+var chaine = ('"--------"');
+
+function imprimerCode() {
+	for (i = 0; i < 4; i++) {
+		console.log(maTable[i]);
+	}
+	console.log(chaine);
+}
+
+function incrementerTable() {
+	for (a = 0; a < 4; a++) {
+		imprimerCode();
+		maTable.pop();
+		maTable.unshift(0);
+	}
+}
+
+incrementerTable();
+*/
+
+/* Version QUI MARCHE */
+var maTable = [1, 0, 0, 0];
+var chaine = ('"--------"');
+
+function imprimerCode() {
+	console.log(maTable[0]);
+	console.log(maTable[1]);
+	console.log(maTable[2]);
+	console.log(maTable[3]);
+	console.log(chaine);
+}
+
+
+function incrementerTable() {
+	for (a = 0; a < 4; a++) {
+		imprimerCode();
+		maTable.pop();
+		maTable.unshift(0);
+	}
+}
+
+incrementerTable();
+
+
+
 
 // END
