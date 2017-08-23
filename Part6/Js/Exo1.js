706adc0b66850551bb1049da7ee2ffdcae46a081
RenaudBernard59 <renaud.bernard@p59production.com>
2017-08-23 16:05:50 +0200

    Editing Exo1.Js but it have bogues rm .Exo1.js.swp !

--- Part6/Js/Exo1.js
+++ Part6/Js/Exo1.js
@@ -1,38 +1,48 @@
 /* ========== Partie 6 - Exercice 1 ========== */
-
-
 // Constructeur de l'objet newPerson
-function  newPerson() = {
-	// Initialisation des Propriétés
-	prenom: "",
-	nom: "",
-	paysResidence: "",
-	age: number,
-	moyensPaiement: [],
 
-	// Initialisation des Méthodes
-	saidPrenom: function() = {
-		window.alert(this.prenom);
-	},
-	saidNom: function() = {
-		window.alert(this.nom);
-	},
-	anneeNaissance: function() {
-		anneeActuel = 2017;
-		anneeNaissance = anneeActuel - this.age;
-		window.alert(anneeNaissance);
-	}	
-};
+function newPerson() {
+  // Initialisation des Propriétés
+  prenom: '',
+  nom: '',
+  paysResidence: '',
+  age: 0,
+  moyensPaiement: [""],
+  // Initialisation des Méthodes
+  saidPrenom: function() = {
+    window.alert(this.prenom);
+  }, //END Function saidPrenom
+  saidNom: function() = {
+    window.alert(this.nom);
+  }, //END Function saidNom
+  anneeNaissance: function() {
+    anneeActuel = 2017;
+    anneeNaissance = anneeActuel - this.age;
+    window.alert(anneeNaissance);
+  } //END Function anneeNaissance
+}; //END Function newPerson
 
 //=====================================================================
+// Création de personnages
+var roger = new newPerson("Roger", "LeTavernier", "Monderalcore", 36, [
+ "Liquide",
+  "Chèque",
+  "CarteBleue",
+  "Paypal",
+  "Bitoins",
+  "Troc"]);
 //=====================================================================
-
-var roger = new newPerson(Roger, LeTavernier, Monderalcore, 36, [Liquide, Chèque, Carte Bleue, Paypal, Bitoins, Troc])
-
-
-
-
-
-
-
+// Apel de fonctions
+roger.saidPrenom;
 // END
+
+
+
+
+
+
+
+
+
+
+
