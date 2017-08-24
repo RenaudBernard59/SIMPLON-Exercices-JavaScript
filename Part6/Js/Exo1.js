55f32473735c50f5eafbf576f88be5255054feed
RenaudBernard59 <renaud.bernard@p59production.com>
2017-08-24 14:07:35 +0200

    Exo1.1 Finish

--- Part6/Js/Exo1.js
+++ Part6/Js/Exo1.js
@@ -1,18 +1,15 @@
 /* ========== Partie 6 - Exercice 1 ========== */
-// Constructeur de l'objet newPerson
-
-function newPerson() {
-  // Initialisation des Propriétés
-  prenom: '',
-  nom: '',
-  paysResidence: '',
-  age: 0,
-  moyensPaiement: [""],
+var roger = {
+  prenom: 'Roger',
+  nom: 'LeTaverier',
+  paysResidence: 'Monderalcore',
+  age: 36,
+  moyensPaiement: ["Liquide", "Carte Bleue", "Chèque", "Paypal" ],
   // Initialisation des Méthodes
-  saidPrenom: function() = {
+  saidPrenom: function() {
     window.alert(this.prenom);
   }, //END Function saidPrenom
-  saidNom: function() = {
+  saidNom: function() {
     window.alert(this.nom);
   }, //END Function saidNom
   anneeNaissance: function() {
@@ -20,20 +17,16 @@ function newPerson() {
     anneeNaissance = anneeActuel - this.age;
     window.alert(anneeNaissance);
   } //END Function anneeNaissance
-}; //END Function newPerson
+}; //END Function roger
 
 //=====================================================================
-// Création de personnages
-var roger = new newPerson("Roger", "LeTavernier", "Monderalcore", 36, [
- "Liquide",
-  "Chèque",
-  "CarteBleue",
-  "Paypal",
-  "Bitoins",
-  "Troc"]);
 //=====================================================================
 // Apel de fonctions
-roger.saidPrenom;
+
+roger.saidNom();
+roger.saidPrenom();
+roger.anneeNaissance();
+
 // END
 
 
