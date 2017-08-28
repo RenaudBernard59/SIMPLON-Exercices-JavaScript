317c79515ae8fdbda0ce2a99381761836681de2b
RenaudBernard59 <renaud.bernard@p59production.com>
2017-08-28 15:14:54 +0200

    Creation skull of constructor

--- Part6/Js/Exo2.js
+++ Part6/Js/Exo2.js
@@ -1,5 +1,24 @@
 /* ========== Partie 6 - Exercice 2 ========== */
+//Initialisation du constructeur
+var nouveauGuerrier = function() {
+	nom: "",
+	attaque: 0,
+	defense: 0,
+	pv: 0,
+	bagarre: function() {
+	var pvAutre, defenseAutre; 
+	var random;
+	var rand1, rand2
+	//TEMP
+	rand1 = 0.5
+	rand2 = 0.3
 
+	//ET	
+	pvAutre = pvAutre - (rand1 * this.attaque) - (rand2 * defense);
+	return pvAutre
+ } //END FUNC Arme
+
+}
 
 
 
