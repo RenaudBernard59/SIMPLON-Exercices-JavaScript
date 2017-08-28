ed16b370891ee7dff7df88be3a4f4bd033aeb5cd
RenaudBernard59 <renaud.bernard@p59production.com>
2017-08-28 10:37:22 +0200

    Merge Exo1 and Exo1a Js files

--- Part6/Js/Exo1.js
+++ Part6/Js/Exo1.js
@@ -21,6 +21,41 @@ var roger = {
 
 //=====================================================================
 
+
+
+var moyensPaiement = ['LIQUIDE', 'CARTE BLEUE', 'CHEQUE', 'PAYPAL'];
+var paiementAutorises = ['CARTE BLEUE', 'PAYPAL', 'BITCOINS', 'RIB'];
+var tb1 = moyensPaiement;
+var tb2 = paiementAutorises;
+var tb1l = tb1.length;
+var resultat = [];
+
+// Fonction pour rechercher dans le tableau
+for (var i = 0; i < (tb1l); i++) {
+  var resulTest;
+  // On parcours le tableau1
+  resulTest = tb2.indexOf(tb1[i]);
+  if (resulTest >= 0) {
+    // Concordance
+    resultat.push(tb1[i]);
+  }
+ } //END conditionnel
+} //END boucle
+
+console.log('Resultat : ' + resultat);
+
+
+
+
+
+
+
+
+
+
+
+
+//=====================================================================
 var itsAllowed = {
 	paysAutorises: ['FRANCE', 'BELGIQUE', 'ALLEMAGNE', 'ROYAUME-UNI', 'ESPAGNE', 'PORTUGAL', 'ITALIE', 'IRLANDE', 'SUISSE'],
 	paiementAutorises: ['CARTE BLEUE', 'PAYPAL', 'BITCOINS', 'RIB'],
