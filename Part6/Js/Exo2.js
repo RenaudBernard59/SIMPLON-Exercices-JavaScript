0fecff43ee3300636542a3662440bcdb7954e3dd
RenaudBernard59 <renaud.bernard@p59production.com>
2017-08-29 09:52:49 +0200

    Upgrading of Part6Exo2

--- Part6/Js/Exo2.js
+++ Part6/Js/Exo2.js
@@ -1,26 +1,31 @@
 /* ========== Partie 6 - Exercice 2 ========== */
 //Initialisation du constructeur
-var nouveauGuerrier = function() {
-	nom: "",
-	attaque: 0,
-	defense: 0,
-	pv: 0,
-	bagarre: function() {
-	var pvAutre, defenseAutre; 
-	var random;
-	var rand1, rand2
-	//TEMP
-	rand1 = 0.5
-	rand2 = 0.3
-
-	//ET	
-	pvAutre = pvAutre - (rand1 * this.attaque) - (rand2 * defense);
+ function  NouveauGuerrier(nom, attaque, defense, pv, attaqueAutre, defenseAutre, pvAutre){
+	this.nom: "",
+	this.attaque: 0,
+	this.defense,
+	this.pv: 0,
+	//Méthodes
+	this.random: function () {
+	var random = Math.round(Math.random*100/100);
+	return random;
+	}, //END FUNC random
+	this.aLaBataille function() {
+	var rand1 = this.random();
+	var rand2 = this.random();
+	pvAutre = this.pvAutre - (rand1 * this.attaque) - (rand2 * this.defenseAutre);
 	return pvAutre
  } //END FUNC Arme
 
 }
 
+//Inetanciation des guerriers
 
+var conan = new NouveauGuerrier("Conan", 60, 60, 120);
+var dovakin = new NouveauGierrier("Dovakin", 80, 30, 140);
+
+
+// Appel des fonctions
 
 
 
